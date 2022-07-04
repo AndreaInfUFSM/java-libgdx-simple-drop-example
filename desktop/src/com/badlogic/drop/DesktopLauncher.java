9daf4be260a36dc24e9239a7392d8bcb29b5ce53
AndreaInfUFSM <andrea@inf.ufsm.br>
2022-07-04 06:18:51 -0300

    Update libGDX project

--- desktop/src/com/badlogic/drop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -1,8 +1,8 @@
-package com.mygdx.drop;
+package com.badlogic.drop;
 
-import com.badlogic.drop.Drop;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.drop.Drop;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
